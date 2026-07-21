ea26032b2965cbf3b77b248e44bfa2554f8f198a
Harleen-Kaur01 <beingharleenkaur01@gmail.com>
2026-07-21 00:31:03 -0700

    Delete EXPERIMENT-3/experiment_3.2.sql

--- EXPERIMENT-3/experiment_3.2.sql
+++
@@ -1,6 +0,0 @@
-/*Implement SELECT queries to filter, group, and sort data on employee dataset */
-
-SELECT emp_name, department, salary
-FROM employee
-WHERE salary > 50000
-ORDER BY department, salary DESC;
